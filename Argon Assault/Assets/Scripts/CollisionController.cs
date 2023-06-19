ada3c35a71745f7692f03dfc228a195eb16a583e
Gherghelejiu <gherg001@gmail.com>
2023-06-19 21:31:18 +0300

    created the explosion particle system

---
+++ Argon Assault/Assets/Scripts/CollisionController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionController : MonoBehaviour
+{
+
+
+    [SerializeField] ParticleSystem crashVfx;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        Debug.Log($"{this.name} triggered by {other.gameObject.name}");
+        StartCrashSequence();
+    }
+
+    private void StartCrashSequence()
+    {
+        crashVfx.Play();
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        gameObject.GetComponent<PlayerController>().enabled = false;
+        Invoke(nameof(ReloadScene), 1.5f);
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"{this.name} collided with {collision.gameObject.name}");
+    }
+}
